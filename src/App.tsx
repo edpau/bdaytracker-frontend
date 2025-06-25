4344f28bdbd4a4bc9df9941412ee0a102b77ed01
edpau <edpau@me.com>
2025-06-25 14:05:20 +0100

    feat: integrate Tailwind CSS using @tailwindcss/vite and downgrade Vite to 6.3.5 for compatibility

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
-
-
-
-
 function App() {
 
   return (
-    <>
-        <p>Hello</p>
-    </>
+   <div className="min-h-screen bg-gray-100 text-center p-8">
+      <h1 className="text-3xl font-bold underline text-blue-600">
+        Hello Tailwind!
+      </h1>
+    </div>
   )
 }
 
