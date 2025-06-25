bd189e071dc1e9c286b9776c2125482d39091aa3
edpau <edpau@me.com>
2025-06-25 15:46:23 +0100

    test: husky hook live

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ return (
   <div className='bg-gray-100 text-center  p-8 min-h-screen'>
   <h1 className="underline text-blue-600  text-3xl font-bold">
   Hello Tailwind!
-  </h1>
+    </h1>
   </div>
 )
 }
