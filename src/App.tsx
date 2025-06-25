5fbf328c6a449f64fe2a6119ffc2861d7e1cb8d4
edpau <edpau@me.com>
2025-06-25 14:58:11 +0100

    chore: set up Prettier with Tailwind CSS plugin for class sorting

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 function App() {
-
   return (
-   <div className="min-h-screen bg-gray-100 text-center p-8">
-      <h1 className="text-3xl font-bold underline text-blue-600">
+    <div className="min-h-screen bg-gray-100 p-8 text-center">
+      <h1 className="text-3xl font-bold text-blue-600 underline">
         Hello Tailwind!
       </h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
