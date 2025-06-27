d4ee1f51a26492f9b5b32106ca1d401f572b1aad
edpau <edpau@me.com>
2025-06-27 18:15:09 +0100

    feat: display birthdays by selected day with dynamic staff list

--- src/App.tsx
+++ src/App.tsx
@@ -5,28 +5,30 @@ interface Staff {
   id: number;
   firstName: string;
   lastName: string;
-  birthday: string;
 }
 
 function App() {
-  const [staffList, setStaffList] = useState<Staff[]>([]);
+  const [staffByDay, setStaffByDay] = useState<Staff[][]>([]);
+  const [currentDateIndex, setCurrentDateIndex] = useState<number>(0);
 
   useEffect(() => {
     // TODO: move fetch logic into api.ts and add loading/error states
     const useMock = import.meta.env.VITE_USE_MOCK === 'true';
     const baseURL = import.meta.env.VITE_API_BASE_URL;
 
-    const url = useMock ? '/mock/staff/test.json' : `${baseURL}/v1/staff`;
+    const url = useMock
+      ? '/mock/staff/mock-staff-per-day.json'
+      : `${baseURL}/v1/staff`;
 
     const fetchData = async () => {
       try {
         const response = await fetch(url);
-        const json: Staff[] = await response.json();
+        const json: Staff[][] = await response.json();
         console.log(json);
         if (!Array.isArray(json)) {
           throw new Error('API did not return an array');
         }
-        setStaffList(json);
+        setStaffByDay(json);
       } catch (error) {
         console.error('Fetch error:', error);
         alert('landing error');
@@ -36,18 +38,41 @@ function App() {
     fetchData();
   }, []);
 
+  const handleSelectDate = (dateIndex: number) => {
+    setCurrentDateIndex(dateIndex);
+    console.log(dateIndex);
+  };
+
   return (
     <div className="min-h-screen bg-gray-100 p-8 text-center">
       <h1 className="text-3xl font-bold text-blue-600 underline">
         Staff Birthday
       </h1>
-      {staffList.map((staff) => (
-        <React.Fragment key={staff.id}>
-          <p>{staff.firstName}</p>
-          <p>{staff.lastName}</p>
-          <p>{staff.birthday}</p>
-        </React.Fragment>
-      ))}
+
+      <h2>{currentDateIndex} birthday star are </h2>
+
+      {!Array.isArray(staffByDay[currentDateIndex]) ||
+      staffByDay[currentDateIndex].length === 0 ? (
+        <p>No one has birthday</p>
+      ) : (
+        staffByDay[currentDateIndex]?.map((staff) => (
+          <ul key={staff.id}>
+            <li>{`${staff.firstName} ${staff.lastName}`}</li>
+          </ul>
+        ))
+      )}
+      <div className="mt-20">
+        {staffByDay.map((_, index) => (
+          <button
+            key={index}
+            type="button"
+            onClick={() => handleSelectDate(index)}
+            className="m-1 border"
+          >
+            {index}
+          </button>
+        ))}
+      </div>
     </div>
   );
 }
